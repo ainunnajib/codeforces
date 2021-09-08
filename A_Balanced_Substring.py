7364094f9c42a89daafd7c41ac06215379962110
Ainun Najib <ainunnajib@gmail.com>
2021-09-08 22:58:22 +0800

    Balanced Substring - trivial solution

--- A_Balanced_Substring.py
+++ A_Balanced_Substring.py
@@ -1,25 +1,13 @@
 t = int(input())
 for _ in range(t):
     n = int(input())
-    s = input()
-    a = [1 if c == 'a' else -1 for c in s]
+    a = [1 if c == 'a' else -1 for c in input()]
     
-    ca = [0 for _ in range(n+1)]
-    ca[0] = 0
-    for i in range(1, n+1):
-        ca[i] = ca[i-1] + a[i-1]
-    
-    seta = {}
     done = False
-    for x in range(1, n+1):
-        if ca[x] == 0:
-            print(1, x)
+    for i in range(n-1):
+        if a[i] + a[i+1] == 0:
+            print(i+1, i+2)
             done = True
             break
-        if ca[x] in seta:
-            print(seta[ca[x]]+1, x)
-            done = True
-            break
-        seta[ca[x]] = x
     if not done:
         print(-1, -1)
